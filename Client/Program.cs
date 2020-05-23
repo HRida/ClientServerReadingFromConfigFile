7c47380547be079bc325dbb2aa95c3df6885c800
HRida <H.Rida1@hotmail.com>
2020-05-23 23:17:49 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Configuration;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("localhost", 2055);
+            try
+            {
+                Stream s = client.GetStream();
+                StreamReader sr = new StreamReader(s);
+                StreamWriter sw = new StreamWriter(s);
+                sw.AutoFlush = true;
+                Console.WriteLine(sr.ReadLine());
+                while (true)
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    sw.WriteLine(name);
+                    if (name == "") break;
+                    Console.WriteLine(sr.ReadLine());
+                }
+                s.Close();
+            }
+            finally
+            {
+                client.Close();
+            } 
+        }
+    }
+}
